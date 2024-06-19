b91139459a74a492b3adf7e2b87b12183295458a
C. Berk Cetin <cberkcetin@gmail.com>
2024-06-19 15:21:26 +0300

    fix(RSS): change query to fetch multiple feed
    Since we will spawn multiple goroutines to scrape for feeds
    We will work on multiple feeds

--- internal/database/feed.sql.go
+++ internal/database/feed.sql.go
@@ -88,26 +88,42 @@ func (q *Queries) GetFeeds(ctx context.Context) ([]Feed, error) {
 	return items, nil
 }
 
-const getNextFetch = `-- name: GetNextFetch :one
+const getNextFeedsToFetch = `-- name: GetNextFeedsToFetch :many
 SELECT id, created_at, updated_at, deleted, name, url, user_id, last_fetched_at FROM feed
 ORDER BY last_fetched_at ASC NULLS FIRST
-LIMIT 1
+LIMIT $1
 `
 
-func (q *Queries) GetNextFetch(ctx context.Context) (Feed, error) {
-	row := q.db.QueryRowContext(ctx, getNextFetch)
-	var i Feed
-	err := row.Scan(
-		&i.ID,
-		&i.CreatedAt,
-		&i.UpdatedAt,
-		&i.Deleted,
-		&i.Name,
-		&i.Url,
-		&i.UserID,
-		&i.LastFetchedAt,
-	)
-	return i, err
+func (q *Queries) GetNextFeedsToFetch(ctx context.Context, limit int32) ([]Feed, error) {
+	rows, err := q.db.QueryContext(ctx, getNextFeedsToFetch, limit)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Feed
+	for rows.Next() {
+		var i Feed
+		if err := rows.Scan(
+			&i.ID,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+			&i.Deleted,
+			&i.Name,
+			&i.Url,
+			&i.UserID,
+			&i.LastFetchedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
 }
 
 const markFeedAsFetched = `-- name: MarkFeedAsFetched :one
--- sql/queries/feed.sql
+++ sql/queries/feed.sql
@@ -6,10 +6,10 @@ RETURNING *;
 -- name: GetFeeds :many
 SELECT * FROM feed;
 
--- name: GetNextFetch :one
+-- name: GetNextFeedsToFetch :many
 SELECT * FROM feed
 ORDER BY last_fetched_at ASC NULLS FIRST
-LIMIT 1;
+LIMIT $1;
 
 -- name: MarkFeedAsFetched :one
 UPDATE feed
